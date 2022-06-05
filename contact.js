772a9d460fe63c28d166fe07f5523b1277dd8b56
MariKutubidze <82153875+Marikutubidze@users.noreply.github.com>
2022-06-05 15:57:54 +0400

    update contact.html

--- contact.js
+++ contact.js
@@ -1,3 +1,22 @@
+// burger element
+
+const icon = document.getElementById("icon");
+const icon1 = document.getElementById("a");
+const icon2 = document.getElementById("b");
+const icon3 = document.getElementById("c");
+const nav = document.getElementById("nav");
+const blue = document.getElementById("blue");
+
+icon.addEventListener("click", function () {
+  icon1.classList.toggle("a");
+  icon1.classList.toggle("active-burger");
+  icon2.classList.toggle("c");
+  icon3.classList.toggle("b");
+  icon3.classList.toggle("active-burger");
+  nav.classList.toggle("show");
+  blue.classList.toggle("slide");
+});
+
 const emailRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 const emailErrorMsg = document.getElementById("emailErrorMsg");
 const submitBtn = document.getElementById("submitBtn");
